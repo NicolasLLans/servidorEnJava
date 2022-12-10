62f0bc3801785d9d698e681190f9e383e297f998
NicolasLLans <94724163+NicolasLLans@users.noreply.github.com>
2022-12-10 20:16:22 -0300

    servidor que recibe solicitudes GET

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import com.sun.net.httpserver.HttpContext;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+
+        final int PUERTO = 9009;
+        HttpServer httpd = HttpServer.create(new InetSocketAddress(PUERTO),0);
+        HttpContext ctx = httpd.createContext("/");
+        ctx.setHandler(Main::gestionarSolicitud);
+        httpd.start();
+    }
+    private static void gestionarSolicitud(HttpExchange exchange) throws IOException{
+        final int CODIGO_RESPUESTA = 200;
+        String contenido = "Respuesta desde el Servidor Http";
+
+        exchange.sendResponseHeaders(CODIGO_RESPUESTA,contenido.getBytes().length);
+        OutputStream os = exchange.getResponseBody();
+
+        os.write(contenido.getBytes());
+        os.close();
     }
 }
