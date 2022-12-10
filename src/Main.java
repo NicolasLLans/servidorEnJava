41fb913928cbbfefdc45e2f1d56446c352ca4178
NicolasLLans <94724163+NicolasLLans@users.noreply.github.com>
2022-12-10 20:44:01 -0300

    Función de manejo de solicitud en el servidor

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ import com.sun.net.httpserver.HttpServer;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
+import java.net.URI;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -18,11 +19,27 @@ public class Main {
     private static void gestionarSolicitud(HttpExchange exchange) throws IOException{
         final int CODIGO_RESPUESTA = 200;
         String contenido = "Respuesta desde el Servidor Http";
-
+        mostrarInformacionSolicitud(exchange);
         exchange.sendResponseHeaders(CODIGO_RESPUESTA,contenido.getBytes().length);
         OutputStream os = exchange.getResponseBody();
 
         os.write(contenido.getBytes());
         os.close();
+    }
+
+    private static void mostrarInformacionSolicitud(HttpExchange exchange){
+
+        System.out.println("Encabezados:");
+        exchange.getRequestHeaders().entrySet().forEach(System.out::println);
+
+        System.out.println();
+
+        System.out.println("Métodos: " + exchange.getRequestMethod());
+
+        System.out.println();
+
+        System.out.println("Query:");
+        URI uri = exchange.getRequestURI();
+        System.out.println(uri.getQuery());
     }
 }
